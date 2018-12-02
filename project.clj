acc211ac4f106ed67b98099ffca63333db8c6c59
Thomas Spellman <thomas@thosmos.com>
2018-12-02 15:39:16 -0800

    Version 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thosmos/mysql-to-datomic "0.3.3-SNAPSHOT"
+(defproject thosmos/mysql-to-datomic "0.3.3"
   :description "MySQL-to-Datomic"
   :url "https://github.com/thosmos/mysql-to-datomic"
   :license {:name "Eclipse Public License"
